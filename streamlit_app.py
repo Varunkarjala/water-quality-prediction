f56a783d68b47ff5b445eb0eed9b82ee55da81df
Varunkarjala <varunkarjala001@gmail.com>
2025-07-04 21:15:04 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,14 +2,13 @@
 import pandas as pd
 import numpy as np
 import joblib
-import pickle
 import streamlit as st
 
 # Load the model and structure
 model = joblib.load("pollution_model.pkl")
 model_cols = joblib.load("model_columns.pkl")
 
-# Let's create an User interface
+# Let's create a User interface
 st.title("Water Pollutants Predictor")
 st.write("Predict the water pollutants based on Year and Station ID")
 
@@ -19,17 +18,19 @@ station_id = st.text_input("Enter Station ID", value='1')
 
 # To encode and then predict
 if st.button('Predict'):
-    if not station_id:
+    if not station_id.strip():
         st.warning('Please enter the station ID')
     else:
         # Prepare the input
-        input_df = pd.DataFrame({'year': [year_input], 'id':[station_id]})
+        input_df = pd.DataFrame({'year': [year_input], 'id':[station_id.strip()]})
         input_encoded = pd.get_dummies(input_df, columns=['id'])
 
         # Align with model cols
         for col in model_cols:
             if col not in input_encoded.columns:
                 input_encoded[col] = 0
+
+        # Ensure the order of columns is same as model expects
         input_encoded = input_encoded[model_cols]
 
         # Predict
@@ -37,6 +38,5 @@ if st.button('Predict'):
         pollutants = ['O2', 'NO3', 'NO2', 'SO4', 'PO4', 'CL']
 
         st.subheader(f"Predicted pollutant levels for the station '{station_id}' in {year_input}:")
-        predicted_values = {}
         for p, val in zip(pollutants, predicted_pollutants):
-            st.write(f'{p}:{val:.2f}')
+            st.write(f'{p}: {val:.2f}')
